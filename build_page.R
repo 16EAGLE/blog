c1993afcde66fd569917737be7c5e3dde823f1df
16EAGLE <jxsw@web.de>
2022-03-08 10:01:36 +0100

    adjusted menu manipulation

--- build_page.R
+++ build_page.R
@@ -1,6 +1,7 @@
 # build site
 cat("Build site\n")
-blogdown::build_site(build_rmd = T)
+#options("blogdown.method" = "html")
+blogdown::build_site(run_hugo = T, build_rmd = F, local = T)
 cat("Serve site\n")
 blogdown::serve_site()
 
@@ -8,18 +9,29 @@ blogdown::serve_site()
 cat("Read config.toml\n")
 config <- readLines("config.toml")
 menu <- gsub('\"', "", gsub('identifier = \"', "", trimws(config[which(sapply(config, function(x) grepl("menu.main", x), USE.NAMES = F))+1])))
-menu <- menu[!(menu == "blog" | menu == "about")]
+menu <- menu[menu != "blog" & menu != "about"]
 files <- paste0(getwd(), "/public/", menu, "/index.html")
+if(any(file.exists(files))){
 
-# fix menu
-cat("Add html tags\n")
-catch <- mapply(x = files, y = menu, function(x, y){
+  # fix menu
+  cat("Add html tags\n")
+  catch <- mapply(x = files, y = menu, function(x, y){
+    z <- readLines(x)
+    i <- grep(paste0('<a  href="/', y, '">', y, '</a>'), z)
+    z[i] <- paste0('<a  class="active" href="/', y, '">', y, '</a>')
+    writeLines(z, x)
+  })
+  
+  # about case
+  x <- paste0(getwd(), "/public/index.html")
   z <- readLines(x)
-  i <- grep(paste0('<a  href="/', y, '">', y, '</a>'), z)
-  z[i] <- paste0('<a  class="active" href="/', y, '">', y, '</a>')
+  i <- grep(paste0('<a  href="/">about</a>'), z)
+  z[i] <- paste0('<a class="active" href="/">about</a>')
   writeLines(z, x)
-})
-
+  
+}else{
+  stop("Files not found!")
+}
 # close down
 cat("Stop serve\n")
 blogdown::stop_server()
